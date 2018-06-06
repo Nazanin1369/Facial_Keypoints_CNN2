44b40853a31f582ec2b0c8531f7bdd6f42809898
nazanin1369 <nazanin1427@gmail.com>
2018-06-06 01:15:15 -0700

    Data Analysis and model definitionwq

---
+++ models.py
@@ -0,0 +1,115 @@
+## Define the convolutional neural network architecture
+
+import torch
+from torch.autograd import Variable
+import torch.nn as nn
+import torch.nn.functional as F
+# can use the below import should you choose to initialize the weights of your Net
+import torch.nn.init as I
+
+
+class Net(nn.Module):
+
+    def __init__(self):
+        super(Net, self).__init__()
+        
+        ## TODO: Define all the layers of this CNN, the only requirements are:
+        ## 1. This network takes in a square (same width and height), grayscale image as input
+        ## 2. It ends with a linear layer that represents the keypoints
+        ## it's suggested that you make this last layer output 136 values, 2 for each of the 68 keypoint (x, y) pairs
+        
+        # As an example, you've been given a convolutional layer, which you may (but don't have to) change:
+        # 1 input image channel (grayscale), 32 output channels/feature maps, 5x5 square convolution kernel
+        
+        
+        ## Note that among the layers to add, consider including:
+        # maxpooling layers, multiple conv layers, fully-connected layers, and other layers (such as dropout or batch normalization) to avoid overfitting
+        
+          
+        # 1 input image channel (grayscale), 32 output channels/feature maps
+        # 5x5 square convolution kernel
+        ## output size = (W-F)/S +1 = (224-5)/1 +1 = 26
+        # the output Tensor for one image, will have the dimensions: (32, 26, 26)
+        # after one pool layer, this becomes (32, 13, 13)
+        
+        self.conv1 = nn.Conv2d(1, 32, 5)
+        
+        
+        # (110 - 5)/ 1 + 1 = 106
+        # (64, 106, 106)
+        # After maxpooling (64, 53, 53)
+        # Prev try
+        self.conv2 = nn.Conv2d(32, 64, 5)
+        #self.conv2 = nn.Conv2d(32, 48, 5)
+        
+        # (53 - 5)/ 1 + 1 = 49
+        # (128, 49, 49)
+        # After maxpooling (64, 24, 24)
+        # Prev try
+        self.conv3 = nn.Conv2d(64, 128, 5)
+        #self.conv3 = nn.Conv2d(48, 64, 5)
+        
+        # (24 - 5)/ 1 + 1 = 20
+        # (256, 20, 20)
+        # After maxpooling (256, 10, 10)
+        #self.conv4 = nn.Conv2d(128, 256, 5)
+        
+        ## Note that among the layers to add, consider including:
+        # maxpooling layers, multiple conv layers, fully-connected layers, and other layers (such as dropout or batch normalization) to avoid overfitting
+        
+        self.pool1 = nn.MaxPool2d(2, 2)
+        self.pool2 = nn.MaxPool2d(2, 2)
+        self.pool3 = nn.MaxPool2d(2, 2)
+        #self.pool4 = nn.MaxPool2d(2, 2)
+        
+        self.dropout1 = nn.Dropout(p=0.4)
+        self.dropout2 = nn.Dropout(p=0.4)
+        self.dropout3 = nn.Dropout(p=0.4)
+    
+        # 20 outputs * the 5*5 filtered/pooled map size
+        self.fc1 = nn.Linear(128*24*24, 1000)
+        
+        # dropout with p=0.4
+        # change dropout from 0.4 to 0.3
+        self.fc1_drop = nn.Dropout(p=0.4)
+        
+        #self.fc2 = nn.Linear(5000, 1000)
+        
+        #self.fc2_drop = nn.Dropout(p=0.4)
+        
+        
+        # finally, create 10 output channels (for the 10 classes)
+        self.fc3 = nn.Linear(1000, 136)
+                    
+
+    def forward(self, x):
+        ## TODO: Define the feedforward behavior of this model
+        ## x is the input image and, as an example, here you may choose to include a pool/conv step:
+        ## x = self.pool(F.relu(self.(x)))
+        
+        x = self.pool1(F.relu(self.conv1(x)))
+        x = self.dropout1(x)
+        
+        x = self.pool2(F.relu(self.conv2(x)))
+        x = self.dropout2(x)
+        
+        x = self.pool3(F.relu(self.conv3(x)))
+        x = self.dropout3(x)
+        
+        #x = self.pool4(F.relu(self.conv4(x)))
+        #x = self.dropout(x)
+        
+        x = x.view(x.size(0), -1)
+        
+        # two linear layers with dropout in between
+        # Removed RELU layer
+        x = F.relu(self.fc1(x))
+        x = self.fc1_drop(x)
+        
+        #x = F.relu(self.fc2(x))
+        #x = self.fc2_drop(x)
+        
+        x = self.fc3(x)        
+        
+        # a modified x, having gone through all the layers of your model, should be returned
+        return x
